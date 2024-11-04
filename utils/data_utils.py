11b572624fae34500023eebcd2dbfe5275c5987e
Woongoh Cho <whdnddh@naver.com>
2024-11-05 01:33:28 +0900

    fix slow loader

--- utils/data_utils.py
+++ utils/data_utils.py
@@ -9,19 +9,27 @@ import numpy as np
 
 class CameraDataset(Dataset):
     
-    def __init__(self, viewpoint_stack, white_background):
+    def __init__(self, viewpoint_stack, white_background, view_only=False, rgba=False):
         self.viewpoint_stack = viewpoint_stack
         self.bg = np.array([1,1,1]) if white_background else np.array([0, 0, 0])
+        self.view_only = view_only
+        self.rgba = rgba
         
     def __getitem__(self, index):
         viewpoint_cam = self.viewpoint_stack[index]
+        if self.view_only:
+            return viewpoint_cam.image, viewpoint_cam
         if viewpoint_cam.meta_only:
-            with Image.open(viewpoint_cam.image_path) as image_load:
-                im_data = np.array(image_load.convert("RGBA"))
-            norm_data = im_data / 255.0
-            arr = norm_data[:,:,:3] * norm_data[:, :, 3:4] + self.bg * (1 - norm_data[:, :, 3:4])
-            image_load = Image.fromarray(np.array(arr*255.0, dtype=np.byte), "RGB")
-            resized_image_rgb = PILtoTorch(image_load, viewpoint_cam.resolution)
+            if self.rgba:
+                with Image.open(viewpoint_cam.image_path) as image_load:
+                    im_data = np.array(image_load.convert("RGBA"))
+                norm_data = im_data / 255.0
+                arr = norm_data[:,:,:3] * norm_data[:, :, 3:4] + self.bg * (1 - norm_data[:, :, 3:4])
+                image_load = Image.fromarray(np.array(arr*255.0, dtype=np.byte), "RGB")
+                resized_image_rgb = PILtoTorch(image_load, viewpoint_cam.resolution)
+            else:
+                image_load = Image.open(viewpoint_cam.image_path)
+                resized_image_rgb = PILtoTorch(image_load, viewpoint_cam.resolution)
             viewpoint_image = resized_image_rgb[:3, ...].clamp(0.0, 1.0)
             if resized_image_rgb.shape[1] == 4:
                 gt_alpha_mask = resized_image_rgb[3:4, ...]
