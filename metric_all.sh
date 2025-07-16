706816522c4d0d69a6824b0068011e7573d700fe
Woongoh Cho <whdnddh@naver.com>
2025-07-16 13:16:29 +0900

    fix: update render command in metric_all.sh to use loaded checkpoint path

--- metric_all.sh
+++ metric_all.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Usage: ./metric_all.sh <model_path> <mask_path> <iteration>
 echo "Running metrics for model: ${1}, mask: ${2}, iteration: ${3}"
-python render.py -m ${1} --skip_train --iteration ${3}
+python render.py -m ${1} --skip_train --loaded_pth ${1}/chkpnt${3}.pth
 python metrics.py -m ${1}
 python metrics_mask.py -m ${1} --mask_path ${2} --gt_path ${1}/test/ours_None/gt
 python metric_fps.py -m ${1}
